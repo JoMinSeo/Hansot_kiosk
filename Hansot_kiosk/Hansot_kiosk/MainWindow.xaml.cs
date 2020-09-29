ec4db98f18f6f5f35a465e8e346b93dbd93df093
hvboq <50200488+hvboq@users.noreply.github.com>
2020-09-29 10:51:41 +0900

    [add] 기초 구조

--- Hansot_kiosk/Hansot_kiosk/MainWindow.xaml.cs
+++ Hansot_kiosk/Hansot_kiosk/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace Hansot_kiosk
         public MainWindow()
         {
             InitializeComponent();
+
+            ProMet.Play();
+        }
+
+        private void ProMet_MediaEnded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProMet_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void ProMet_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            this.playTimeSlider.Minimum = 0;
+
+            this.playTimeSlider.Maximum = this.mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.ProMet.Source == null)
+
+            {
+
+                return;
+
+            }
+            this.ProMet.Play();
         }
     }
 }
