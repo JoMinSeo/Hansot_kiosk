043b10fd72926e4a9a9652cbb5f1a9ab089a0dff
hvboq <50200488+hvboq@users.noreply.github.com>
2020-10-06 12:16:01 +0900

    [Add] Assets 추가, gitignore 추가

---
+++ Hansot_kiosk/Hansot_kiosk/ViewModel/OrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Hansot_kiosk.Model;
+using System.Collections.Generic;
+
+namespace Hansot_kiosk.ViewModel
+{
+    public class OrderViewModel
+    {
+        public List<Menu> lstMenu { get; set; }
+
+        public OrderViewModel()
+        {
+        }
+    }
+}
