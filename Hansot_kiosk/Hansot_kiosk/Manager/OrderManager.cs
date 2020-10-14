9110e5d981062c9e84cdf02518e3035642f80205
hvboq <50200488+hvboq@users.noreply.github.com>
2020-10-14 13:40:38 +0900

    정리

--- Hansot_kiosk/Hansot_kiosk/Manager/OrderManager.cs
+++ Hansot_kiosk/Hansot_kiosk/Manager/OrderManager.cs
@@ -21,37 +21,37 @@ namespace Hansot_kiosk.Manager
             {
                 Category = Category.meatmeat,
                 Name = "고기고기",
-                Path = @"/Assets/Menu/meatmeat/고기고기.jpg"
+                Path = @"Assets/Menu/meatmeat/고기고기.jpg"
             });
             lstMenu.Add(new Menu()
             {
                 Category = Category.meatmeat,
                 Name = "돈까스고기고기",
-                Path = @"/Assets/Menu/meatmeat/돈까스도련님고기고기.jpg"
+                Path = @"Assets/Menu/meatmeat/돈까스도련님고기고기.jpg"
             });
             lstMenu.Add(new Menu()
             {
                 Category = Category.meatmeat,
                 Name = "돈치고기고기",
-                Path = @"/Assets/Menu/meatmeat/돈치 고기고기.jpg"
+                Path = @"Assets/Menu/meatmeat/돈치 고기고기.jpg"
             });
             lstMenu.Add(new Menu()
             {
                 Category = Category.meatmeat,
                 Name = "새치고기고기",
-                Path = @"/Assets/Menu/meatmeat/새치 고기고기.jpg"
+                Path = @"Assets/Menu/meatmeat/새치 고기고기.jpg"
             });
             lstMenu.Add(new Menu()
             {
                 Category = Category.meatmeat,
                 Name = "생선까스도련님고기고기",
-                Path = @"/Assets/Menu/meatmeat/생선까스도련님고기고기.jpg"
+                Path = @"Assets/Menu/meatmeat/생선까스도련님고기고기.jpg"
             });
             lstMenu.Add(new Menu()
             {
                 Category = Category.meatmeat,
                 Name = "탕수육도련님고기고기",
-                Path = @"/Assets/Menu/meatmeat/탕수육도련님고기고기.jpg"
+                Path = @"Assets/Menu/meatmeat/탕수육도련님고기고기.jpg"
             });
         }
     }
