541608a93789016130d33e4af5d9afd9d5fd5587
hvboq <50200488+hvboq@users.noreply.github.com>
2020-10-23 17:36:43 +0900

    [Add] OrderManager INotifyPropertyChanged 상속받음

--- Hansot_kiosk/Hansot_kiosk/Control/OrderCtrl.xaml.cs
+++ Hansot_kiosk/Hansot_kiosk/Control/OrderCtrl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Hansot_kiosk.Common;
 using Hansot_kiosk.Manager;
+using Hansot_kiosk.Model;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,18 +27,8 @@ namespace Hansot_kiosk.Control
         private void init()
         {
             lbMenus.ItemsSource = OrderMenuManager.ListMenu;
-            lvOrderdMenus.ItemsSource = App.orderManager.orderedMenus;
+            lvOrderdMenus.ItemsSource = App.orderManager.OrderedMenus;
         }
-        private void lbMenus_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (lbMenus.SelectedItems.Count < 1)
-            {
-                return;
-            }
-
-            Model.MenuModel model = lbMenus.SelectedItem as Model.MenuModel;
-            App.orderManager.orderedMenus.Add(model);
-        }  
         private void lbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (lbCategory.SelectedIndex == -1)
@@ -53,6 +44,17 @@ namespace Hansot_kiosk.Control
                 Category category = (Category)lbCategory.SelectedIndex;
                 lbMenus.ItemsSource = OrderMenuManager.ListMenu.Where(x => x.Category == category).ToList();
             }
+        }
+        private void lbMenus_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbMenus.SelectedItems.Count < 1)
+            {
+                return;
+            }
+
+            MenuModel model = lbMenus.SelectedItem as MenuModel;
+            App.orderManager.OrderedMenus.Add(model);
+            lvOrderdMenus.ItemsSource = App.orderManager.OrderedMenus;
         }
     }
 }
--- Hansot_kiosk/Hansot_kiosk/Manager/OrderManager.cs
+++ Hansot_kiosk/Hansot_kiosk/Manager/OrderManager.cs
@@ -1,17 +1,60 @@
-﻿using Hansot_kiosk.Model;
+﻿using Hansot_kiosk.Common;
+using Hansot_kiosk.Model;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Hansot_kiosk.Manager
 {
-    public class OrderManager
+    public class OrderManager : INotifyPropertyChanged
     {
-        public OrderModel currentOrder { get; set; }
-        public List<MenuModel> orderedMenus { get; set; }
+        private OrderModel _currentOrder;
+        public OrderModel CurrentOrder
+        {
+            get { return _currentOrder; }
+            set
+            {
+                _currentOrder = value;
+                OnPropertyChanged(nameof(CurrentOrder));
+            }
+        }
+        private List<MenuModel> _orderedMenus;
+        public List<MenuModel> OrderedMenus
+        {
+            get { return _orderedMenus; }
+            set
+            {
+                _orderedMenus = value;
+                OnPropertyChanged(nameof(OrderedMenus));
+            }
+        }
 
         public OrderManager()
         {
-            currentOrder = new OrderModel();
-            orderedMenus = new List<MenuModel>();
+            CurrentOrder = new OrderModel();
+            OrderedMenus = new List<MenuModel>();
+            //OrderedMenus.Add(new MenuModel()
+            //{
+            //    Category = Category.MEATMEAT,
+            //    Name = "고기고기",
+            //    Path = "Assets/Menu/meatmeat/고기고기.jpg",
+            //    Page = 1
+            //});
+            //OrderedMenus.Add(new MenuModel()
+            //{
+            //    Category = Category.MEATMEAT,
+            //    Name = "돈까스고기고기",
+            //    Path = @"Assets/Menu/meatmeat/돈까스도련님고기고기.jpg",
+            //    Page = 1
+            //});
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propetryName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propetryName));
+            }
         }
     }
 }
