371dc7bb3b3bee54eed66251d13b9390698ac38c
hvboq <50200488+hvboq@users.noreply.github.com>
2020-10-23 18:09:27 +0900

    [Modify] OrderedMenuModel의 Prop 수정

--- Hansot_kiosk/Hansot_kiosk/Model/OrderedMenuModel.cs
+++ Hansot_kiosk/Hansot_kiosk/Model/OrderedMenuModel.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hansot_kiosk.Common;
 
 namespace Hansot_kiosk.Model
 {
     public class OrderedMenuModel
     {
         public int MenuIDX { get; set; }
+        public string MenuName { get; set; }
+        public Category Category { get; set; }
+        public int Price { get; set; }
         public int OrderIdx { get; set; }
         public int Amount { get; set; }
     }
