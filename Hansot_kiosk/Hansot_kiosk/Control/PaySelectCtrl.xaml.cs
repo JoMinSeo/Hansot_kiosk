7f0b0c94bcf82472971045593c48113a3524ab9b
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-06 20:31:18 +0900

    [Add] PaySelectCtrl 주문 메뉴 목록 ListView 추가

--- Hansot_kiosk/Hansot_kiosk/Control/PaySelectCtrl.xaml.cs
+++ Hansot_kiosk/Hansot_kiosk/Control/PaySelectCtrl.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using Kiosk.UIManager;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Hansot_kiosk.Control
 {
@@ -24,6 +12,11 @@ namespace Hansot_kiosk.Control
         public PaySelectCtrl()
         {
             InitializeComponent();
+        }
+
+        private void init()
+        {
+            lvOrderdMenus.ItemsSource = App.orderManager.OrderedMenus;
         }
 
         private void PreviusBtn_Click(object sender, RoutedEventArgs e)
