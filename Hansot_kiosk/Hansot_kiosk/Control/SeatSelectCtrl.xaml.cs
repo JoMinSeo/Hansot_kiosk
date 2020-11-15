234e8180fb77648378ecfef6d8f377427ccd144f
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-15 16:17:16 +0900

    [Add] table model 생성, SeatSelectCtrl listbox 추가

--- Hansot_kiosk/Hansot_kiosk/Control/SeatSelectCtrl.xaml.cs
+++ Hansot_kiosk/Hansot_kiosk/Control/SeatSelectCtrl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Kiosk.UIManager;
+﻿using Hansot_kiosk.Model;
+using Kiosk.UIManager;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -7,13 +10,34 @@ namespace Hansot_kiosk.Control
     /// <summary>
     /// SeatSelectCtrl.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class SeatSelectCtrl : UserControl
+    public partial class SeatSelectCtrl : UserControl, INotifyPropertyChanged
     {
+        private ObservableCollection<TableModel> _tables = new ObservableCollection<TableModel>();
+        public ObservableCollection<TableModel> Tables
+        {
+            get => _tables;
+            set
+            {
+                _tables = value;
+                OnPropertyChanged(nameof(Tables));
+            }
+        }
         public SeatSelectCtrl()
         {
             InitializeComponent();
+            init();
+        }
+        private void init()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                TableModel temp = new TableModel(i);
+                Tables.Add(temp);
+            }
+            this.DataContext = this;
         }
 
+        #region UIControl
         private void PreviusBtn_Click(object sender, RoutedEventArgs e)
         {
             App.uIStateManager.Pop();
@@ -25,5 +49,17 @@ namespace Hansot_kiosk.Control
             if (uc != null)
                 App.uIStateManager.Push(uc);
         }
+        #endregion
+
+        #region PropertyChangedEvent
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propetryName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propetryName));
+            }
+        }
+        #endregion
     }
 }
---
+++ Hansot_kiosk/Hansot_kiosk/Model/TableModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hansot_kiosk.Model
+{
+    public class TableModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        private string _remainingSec;
+        public string RemainingSec
+        {
+            get => _remainingSec;
+            set
+            {
+                _remainingSec = value;
+                OnPropertyChanged(nameof(RemainingSec));
+            }
+        }
+        private string _remainingMin;
+        public string RemainingMin
+        {
+            get => _remainingMin;
+            set
+            {
+                _remainingMin = value;
+                OnPropertyChanged(nameof(RemainingMin));
+            }
+        }
+        public TableModel(int num)
+        {
+            this.Name = num + "번 테이블";
+        }
+
+        #region PropertyChangedEvent
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propetryName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propetryName));
+            }
+        }
+        #endregion
+    }
+}
