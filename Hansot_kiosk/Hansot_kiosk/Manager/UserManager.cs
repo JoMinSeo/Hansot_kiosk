9635fad23b19740cb5a0d1100a4c86a3f9d2761a
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-17 19:28:34 +0900

    [Modify] UserManager 코드정리

--- Hansot_kiosk/Hansot_kiosk/Manager/UserManager.cs
+++ Hansot_kiosk/Hansot_kiosk/Manager/UserManager.cs
@@ -1,10 +1,6 @@
 ﻿using Hansot_kiosk.Model;
 using Hansot_kiosk.Service;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hansot_kiosk.Manager
 {
