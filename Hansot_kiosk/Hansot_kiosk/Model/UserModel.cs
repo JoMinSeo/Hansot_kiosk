4d3bfc69a15c075ce5bab110657ed7f102436a2b
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-22 17:05:28 +0900

    [Remove] UserModel 에서 불필요한 Using 구문 삭제

--- Hansot_kiosk/Hansot_kiosk/Model/UserModel.cs
+++ Hansot_kiosk/Hansot_kiosk/Model/UserModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hansot_kiosk.Model
+﻿namespace Hansot_kiosk.Model
 {
     public class UserModel
     {
