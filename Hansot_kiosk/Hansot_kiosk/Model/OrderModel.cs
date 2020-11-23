6891f22ceec8df42f0cd0d6e1f8e7d117ce19498
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-23 17:54:37 +0900

    [Add] OrderModel Binding을 위한 INotifyPropertyChanged 상속받음

--- Hansot_kiosk/Hansot_kiosk/Model/OrderModel.cs
+++ Hansot_kiosk/Hansot_kiosk/Model/OrderModel.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Hansot_kiosk.Model
 {
-    public class OrderModel
+    public class OrderModel : INotifyPropertyChanged
     {
-        public int IDX { get; set; }
+        public void init()
+        {
+            TotalPrice = 0;
+        }
+        private int _idx;
+        public int IDX
+        {
+            get => _idx;
+            set
+            {
+                _idx = value;
+                OnPropertyChanged(nameof(IDX));
+            }
+        }
         public int User_IDX { get; set; }
         public int Seat_IDX { get; set; }
         public bool IsCard { get; set; }
         public DateTime OrderedTime { get; set; }
+        private int _totalPrice;
+        public int TotalPrice
+        {
+            get => _totalPrice;
+            set
+            {
+                _totalPrice = value;
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
+        #region PropertyChangedEvent
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propetryName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propetryName));
+        }
+        #endregion
+
     }
 }
