dde30c14cfad9a697c3a1adeca074e70bc791c5b
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-23 19:45:11 +0900

    [Add] BoolToBisibilityConverter 추가.

---
+++ Hansot_kiosk/Hansot_kiosk/Converter/BoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Hansot_kiosk.Converter
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = (value is bool) ? (bool)value : false;
+            return flag ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
