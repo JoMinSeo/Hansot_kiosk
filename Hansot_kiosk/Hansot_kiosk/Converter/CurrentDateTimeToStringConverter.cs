1948a3037f0d960e65a3a0331c37a832143870aa
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-24 17:19:07 +0900

    [Add] CurrentDateTimeToStringConverter 추가

---
+++ Hansot_kiosk/Hansot_kiosk/Converter/CurrentDateTimeToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Hansot_kiosk.Converter
+{
+    class CurrentDateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime flag = (value is DateTime) ? (DateTime)value : default(DateTime);
+            string sFlag = String.Format("{0:yyyy년 MM월 dd일 tt hh시 mm분 ss초}", flag);
+            return sFlag;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
