7a9b9e5fb6ece27efe8553578d6232fd91bb7227
hvboq <50200488+hvboq@users.noreply.github.com>
2020-11-26 17:47:25 +0900

    [Remove] OrderedMenu의 불필요한 MenuName 삭제

--- Hansot_kiosk/Hansot_kiosk/Model/OrderedMenuModel.cs
+++ Hansot_kiosk/Hansot_kiosk/Model/OrderedMenuModel.cs
@@ -4,11 +4,11 @@ namespace Hansot_kiosk.Model
 {
     public class OrderedMenuModel
     {
+        public int IDX { get; set; }
+        public int OrderIdx { get; set; }
         public int MenuIDX { get; set; }
-        public string MenuName { get; set; }
         public ECategory Category { get; set; }
         public int Price { get; set; }
-        public int OrderIdx { get; set; }
         public int Amount { get; set; }
     }
 }
