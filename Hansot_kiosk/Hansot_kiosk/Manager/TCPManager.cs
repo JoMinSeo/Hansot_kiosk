0ee9a2c51e5732eed65c3d04d1985d31e03f80b6
Jominseo <minseocho0309@gmail.com>
2020-11-28 16:04:33 +0900

    [update] 서버와 연결이 끊겼을떄 로직 처리

--- Hansot_kiosk/Hansot_kiosk/Manager/TCPManager.cs
+++ Hansot_kiosk/Hansot_kiosk/Manager/TCPManager.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -17,9 +18,13 @@ namespace Hansot_kiosk.Manager
         private bool isSend = false;
         private byte[] sendData = new byte[MAX_LEN];
         private byte[] receiveData = new byte[MAX_LEN];
+        byte[] response = new byte[MAX_LEN];
 
         NetworkStream networkStream = null;
         TcpClient tcpClient = null;
+
+        private Thread tcpThread = null;
+        public bool isConnection = false;
 
         public string PostMessage(TcpModel tcpModel)
         {
@@ -35,19 +40,38 @@ namespace Hansot_kiosk.Manager
                 }
 
                 networkStream.Write(sendData, 0, sendData.Length);
-                byte[] response = new byte[MAX_LEN];
-                Int32 readData = networkStream.Read(response, 0, response.Length);
-
-                String getResponse = Encoding.UTF8.GetString(response, 0, readData);
                 tcpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+
+                this.isConnection = true;
+
+                Int32 readData = networkStream.Read(this.response, 0, this.response.Length);
+                string getResponse = Encoding.UTF8.GetString(response, 0, readData);
                 return getResponse;
             }
             catch(Exception e)
             {
-                MessageBox.Show(e.ToString());
+                this.isConnection = false;
+                MessageBox.Show("서버와 연결이 끊겨있습니다.");
             }
 
             return null;
         }
+
+        //public void threadStart()
+        //{
+        //    if (this.isConnection)
+        //    {
+        //        this.tcpThread = new Thread(new ThreadStart(receiveMessage));
+        //        tcpThread.Start();
+        //    }
+        //}
+
+        //public void threadEnd()
+        //{
+        //    if (tcpThread != null)
+        //    {
+        //        tcpThread.Abort();
+        //    }
+        //}
     }
 }
